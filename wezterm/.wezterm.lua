d5f51692cc49befe0f48d502a55409b9b4fe04ac
Mariusz Malek <34481952+mashimori@users.noreply.github.com>
2025-12-09 20:50:59 +0100

    Add initial wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,41 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 16
+config.font = wezterm.font('CaskaydiaCove Nerd Font', { })
+config.color_scheme = 'Catppuccin Mocha'
+
+config.window_background_opacity = 0.8
+config.text_background_opacity = 1.0
+config.kde_window_background_blur = true
+
+config.enable_tab_bar = false
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
+
+-- Finally, return the configuration to wezterm:
+return config
