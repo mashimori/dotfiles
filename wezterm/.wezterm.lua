46b7105b740c2edccf02313dadfbd401fe249870
Mariusz Malek <34481952+mashimori@users.noreply.github.com>
2025-12-11 19:09:01 +0100

    Small improvements

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,23 +4,26 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
--- or, changing the font size and color scheme.
 config.font_size = 18
 config.font = wezterm.font('CaskaydiaCove Nerd Font', { })
+config.line_height = 1.1
 config.color_scheme = 'Catppuccin Mocha'
 
-config.window_background_opacity = 0.8
+config.default_prog = { '/usr/bin/zsh' }
+config.window_background_opacity = 0.9
 config.text_background_opacity = 1.0
 config.kde_window_background_blur = true
-
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.show_new_tab_button_in_tab_bar = false
 config.enable_tab_bar = false
 
+
+-- Custom right-click behavior: copy selection if present, else paste
 config.mouse_bindings = {
 	{
 		event = { Down = { streak = 1, button = "Right" } },
@@ -37,5 +40,4 @@ config.mouse_bindings = {
 	},
 }
 
--- Finally, return the configuration to wezterm:
 return config
